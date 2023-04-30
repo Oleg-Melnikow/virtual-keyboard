919ea732a227f147c5f35a1f3f8fc72e2bad42c6
Oleg-Melnikow <oleg.melnikau@gmail.com>
2023-04-30 13:17:05 +0300

    feat: add header with info to root element

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const bodyElement = document.querySelector("body");
 const rootElement = document.createElement("div");
 rootElement.classList.add("root");
 
+const header = document.createElement("header");
+header.classList.add("header");
+header.innerHTML = `
+<h1 class="title">Virtual keyboard</h1>
+<div class="info">
+<p>Клавиатура создана в операционной системе Windows</p>
+<p>Для переключения языка используйте следующую комбинацию: левыe ctrl + alt</p>
+</div>
+`;
+
+rootElement.append(header);
 bodyElement.append(rootElement);
 
 console.log(bodyElement);
