558b785ca56fbfd6ea7128508abd2ecd6fa42ccd
Oleg-Melnikow <oleg.melnikau@gmail.com>
2023-04-30 12:37:20 +0300

    feat: add root element into body

---
+++ index.js
@@ -0,0 +1,8 @@
+const bodyElement = document.querySelector("body");
+
+const rootElement = document.createElement("div");
+rootElement.classList.add("root");
+
+bodyElement.append(rootElement);
+
+console.log(bodyElement);
